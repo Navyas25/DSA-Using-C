34056f1eb4ad5d55675a74dbb27dcdc8c238da92
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-01 19:23:35 +0530

    Add array initialization using another array and printing example
    Initialize arr2 using values from arr1 and print them.

---
+++ Array1.c
@@ -0,0 +1,16 @@
+//initialize an array using another array
+#include<stdio.h>
+int main()
+{
+  int arr1[4]={1,2,3,4};
+  int arr2[4];
+  for(int i=0;i<4;i++)
+  {
+    arr2[i]=arr1[i];
+  }
+  for(int i=0;i<4;i++)
+  {
+    printf("%d\n",arr2[i]);
+  }
+  return 0;
+}
