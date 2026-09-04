8ce6877d60a291b6714ef3fd78433a5f060240c2
Navya Bhushan <bhushanavya2@gmail.com>
2026-09-04 09:00:14 +0530

    Create Array5.c

---
+++ Array/Array5.c
@@ -0,0 +1,49 @@
+//add even and odd place numbers of an array
+#include<stdio.h>
+int evenSum(int arr[],int n)
+{
+    int sum=0;
+    for(int i=1;i<n;i+=2)
+        sum+=arr[i];
+    return sum;
+}
+int oddSum(int arr[],int n)
+{
+    int sum=0;
+    for (int i=0;i<n;i+=2)
+        sum+=arr[i];
+        return sum;
+}
+int allSum(int arr[],int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    return sum;
+}
+int main()
+{
+  int a[50],n,choice;
+  printf("Enter the size of the array:");
+  scanf("%d",&n);
+  printf("enter the elements of the array:");
+  for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("enter your choice:\n1.even sum\n2.odd sum\n3.all sum");
+    scanf("%d",&choice);
+    switch(choice){
+        case 1:
+            printf("%d",evenSum(a,n));
+            break;
+        case 2:
+            printf("%d",oddSum(a,n));
+            break;
+        case 3:
+            printf("%d",allSum(a,n));
+            break;
+        default:
+            printf("invalid choice");
+            break;
+    }
+    return 0;
+}
